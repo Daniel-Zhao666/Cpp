a3abf107b919961ef369c19d67001e98b9f64dce
Daniel Zhao <82953862+Daniel-Zhao666@users.noreply.github.com>
2021-08-22 16:10:46 +0800

    Add files via upload
    关于引用的学习

--- learning.cpp
+++ learning.cpp
@@ -1,82 +1,126 @@
 #include<iostream>
-#include<string>
 
-using namespace std;//空间
+using namespace std;
 
-#define Year 365
+//引用的使用场景
+void ShowValue(const int& v)
+{
+	//v = 1;//相当于常量指针，不可对指针指向的地址中存放的值进行修改
+	cout << v << endl;
+}
 
 int main()
 {
+	//int& ref = 1;//引用需要开辟一个合法的空间
 
-	/*cout << "一年天数:" << Year << "天" << endl;//拼接*/
-	/*cout << "hello world!" << endl*/
+	const int& ref = 1;//const后编译器会优化代码，int temp = 1; const int& ref = temp;
 
-	//float f1 = 2.3f;//加 "f" 表示有效数字为单精度,否则可能会导致精度缺失(double -> float)
+	//ref = 2;//错误 --》不可修改值
 
-	//cout << "f1的精度打印" << f1 << endl;
+	//函数中利用const引用，以防止误操作修改实参
+	int a = 10;
+	ShowValue(a);
 
-	//double d1 = 2.312368;//默认情况下为双精度 double
+	system("pause");
+	return 0;
+}
 
-	//cout << "d1的精度显示" << d1 << endl;//默认打印的有效数字为 "6"
+/*void swap1(int* x, int* y)
+{
+	int tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
 
-	/*char ch1 = 'a';//注：只可用单引号
-	char ch2 = 'A';//引号中字符不可多
-	char ch3 = '0';*/
-	/*cout << "char类型的变量内存中所占大小"<< sizeof(char) << endl;*/
+//引用的传递
+void swap2(int& x , int& y)
+{
+	int temp = x;
+	x = y;
+	y = temp;
+}
 
-	/*cout << (int)ch1 << endl;//97(内存中对应的ASCII码值)
-	cout << (int)ch2 << endl;//65
-	cout << (int)ch3 << endl;//48
-	*/
+//引用做函数的返回值
+//注：不要返回局部变量的引用
+int& test1()
+{
+	int a = 1;//局部变量存放在 栈区
+	return a;//返回一个引用的值
+}
+
+//函数的调用可以作为左值
+int& test2()
+{
+	//全局区中的静态变量，在整个程序运行结束后被系统所释放
+	static int a = 1;
+	return a;
+
+}*/
+
+
+
+//int main()
+//{
+//	int a = 1;
+//	int b = 0;
+	//const int* p = &a;//常量指针(常指针)，本质是常量（const在前），这个常量是指针的地址
+	//指针的方向可以被修改， 指针指向的值不可被修改
 	
+	// *p = 2;//错误
+	//p = &b;//正确  指针的指向可以被修改
 
-	/*char str[] = "welcome to CDUT";
-	cout << str << endl;
-	*/
+ //	int* const p1 = &a;//指针常量(本质是指针)，此时const修饰的是指针所指向的地址（不可更改）
+	//即常量的值可以被修改 而指针的方向不可被修改
+  //	*p1 = 1; //正确
 
-	/*string str2 = "hello CDUT";
-	cout << str2 << endl;
-	*/
+	//p1 = &a; // 错误
 
+//	cout << "*p1= " << *p1 << endl;
 
-	//bool flag = true;
-	//cout << flag << endl;
+	//int &ref = test1();
+	//cout << "ref= " << ref << endl;//编译器做了一次保留
+	//cout << "ref= " << ref << endl;//由于a中的局部变量被释放，导致结果出错
 
-	//flag = false;//不能重复定义
-	//cout << flag << endl;
+	/*int& ref = test2();
+	cout << "ref= " << ref << endl;
+	cout << "ref= " << ref << endl;*/
 
-	//cout << "bool所占大小：" << sizeof(flag) << endl;
-
-	//string str;
-	//cout << "请输入字符串的变量:" << endl;
-	//
-	//cin >> str;
-	//cout << str << endl;
-
-	/*bool flag = true;
-	cout << "请输入bool类型的数据" << endl;
-	
-	cin >> flag;
-	cout << flag << endl;*/
+	//函数的引用可以作为左值使用
+	//test2() = 2;
+	//cout << "ref= " << ref << endl;
 
 	/*int a = 1;
-	int b = (a++) * 2;
-	cout << "a的值为=" << a << endl;
-	cout << "b的值=" << b << endl;*/
+	int b = 2;
+	cout << "交换前a = " << a << endl;
+	cout << "交换前b = " << b << endl;*/
 
-	/*
-	int c = 3;
-	int d = (--c) % 3;
-	cout << "c = " << c << endl;
-	cout << "d = " << d << endl;//前置运算为先进行开始字符，后计算表达式，后置运算相反
-	*/
-
-	int a = 1;
-	    a %= 10;
-	cout << "a=" << a << endl;
+	//swap1(&a, &b);
+	//cout << "传址交换后a: " << a << endl;
+	//cout << "传址交换后b: " << b << endl;
 
 
-    system("pause");//暂停
-	return 0;
+	//swap2(a,b);
+	//cout << "引用交换后a: " << a << endl;
+	//cout << "引用交换后b: " << b << endl;
 
-}
+
+
+	//引用必须初始化,初始化后不可以被改变
+
+	//int &b;
+	//int c = 10;
+
+	////引用：  数据类型 &别名 = 原名
+	//int &b = a;
+	//cout << "a= " << a << endl;
+	//cout << "b= " << b << endl;
+
+	//b = 2;
+
+	//cout << "a= " << a << endl;
+	//cout << "b= " << b << endl;
+
+//	system("pause");
+//
+//	return 0;
+//}
