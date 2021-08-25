4ce4ff787431078668ef7104f984c09bce29ba18
Daniel-Zhao666 <2271003121@qq.com>
2021-08-25 13:22:47 +0800

    静态成员与它的函数的学习

---
+++ 静态成员及其函数.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+/*
+class State 
+{
+	//静态成员函特点:
+	//共享一个函数
+	//静态成员函数只能访问 静态成员变量
+
+public:
+	static void add()
+	{
+		cout << "add函数的调用" << endl;
+		A = 10;
+		//B = 2;
+	}
+	static int A;//静态成员变量  类内申明 类外初始化
+	//int B;
+
+private:
+	//静态成员函数有访问权限
+	static void div()
+	{
+		cout << "div函数的调用" << endl;
+	}
+    int  State::A = 1;
+};
+
+void hanshu()
+{
+//静态成员变量的两种访问方式
+
+	//1、创建对象进行访问
+	State sta;
+	sta.add();
+
+	//2、类名进行访问
+	State::add();
+
+	//State::div();//私有权限不可访问
+
+}*/
+
+/*
+int main()
+{
+
+	hanshu();
+
+	system("pause");
+	return 0;
+}*/
